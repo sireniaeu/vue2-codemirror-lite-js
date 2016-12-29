80f78aeb39e86bcc367a0b5bf793e2a0d799c17a
Dan Mindru <mindrudan@gmail.com>
2016-12-29 16:49:59 +0200

    Add simple demo

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var path = require('path')
+var webpack = require('webpack')
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: './demo/bundle.js',
+    libraryTarget: 'var',
+    library: 'CodeMirror'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: path.resolve(__dirname, './'),
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('dev')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new BundleAnalyzerPlugin()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'demo'),
+    compress: true,
+    port: 9000
+  }
+}
