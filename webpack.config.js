0a0571b3dd009838a67c2918ef53ed7e18f5a87d
Michal Kapiczynski <michal.kapiczynski@sirenia.eu>
2018-04-27 10:49:28 +0200

    Changing webpack build in order to use webpack minified js by npm

--- webpack.config.js
+++ webpack.config.js
@@ -3,48 +3,49 @@ var webpack = require('webpack')
 var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
-  entry: './index.js',
-  output: {
-    filename: './demo/bundle.js',
-    libraryTarget: 'var',
-    library: 'CodeMirror',
-    chunkFilename: './demo/[id].js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: path.resolve(__dirname, './'),
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      }
-    ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify('dev')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    // new BundleAnalyzerPlugin() // un-comment to generate bundle report
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, './demo'),
-    compress: true,
-    port: 9000
-  }
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'index.js',
+        libraryTarget: 'umd',
+        library: 'CodeMirror',
+        umdNamedDefine: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: path.resolve(__dirname, './'),
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env':{
+                'NODE_ENV': JSON.stringify('dev')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        // new BundleAnalyzerPlugin() // un-comment to generate bundle report
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, './dist'),
+        compress: true,
+        port: 9000
+    }
 }
